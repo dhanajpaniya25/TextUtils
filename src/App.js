2234c3005ac9253f1a09b2acceee998b18d7291f
Dhanaj Paniya <dhanajpaniya25@gmail.com>
2023-04-21 12:22:58 +0530

    making repository on github

--- src/App.js
+++ src/App.js
@@ -1,31 +1,62 @@
 import './App.css';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import React, { useState } from 'react';
+import Alert from './Components/Alert';
+// import About from './Components/About';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 function App() {
+  const [mode, setMode] = useState('light'); //gives whether the mode is dark or light
+  const [alert, setAlert] = useState(null);
+
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark Mode Has Been Activated", "success");
+      document.title = 'TextUtils - Dark Mode';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode Has Been Activated", "success");
+      document.title = 'TextUtils - Light Mode';
+    }
+  }
+  // function to toggle between dark and light mode for the page
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">ReactApp</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      {/* <Router> */}
+        <Navbar title="TextUtils" aboutapp="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+        {/* <Switch> */}
+          {/* exact is written here because react does not differentitate for links on its on and and can relocate to the same link if you are accessing a link inside that link so to be accurate exact is used for the reference of location */}
+          {/* <Route exact path="/about"> */}
+            {/* <About /> */}
+          {/* </Route> */}
+          {/* <Route exact path="/"> */}
+            <TextForm showAlert={showAlert} heading="Enter The Text To Analyze Below" mode={mode} />
+          {/* </Route> */}
+        {/* </Switch> */}
+      </div>
+    {/* </Router > */}
     </>
   );
 }
-
 export default App;
